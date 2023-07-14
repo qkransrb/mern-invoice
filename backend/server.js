4f8d5c21e42906ea32803bf27a2171eb5a9537ce
qkransrb <qkransrb90@gmail.com>
2023-07-14 16:49:27 +0900

    setup basic express backend api

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import chalk from "chalk";
+import cookieParser from "cookie-parser";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 1997;
+
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  console.log(
+    `${chalk.green.bold("✔")} 👍 Server running in ${chalk.yellow.bold(
+      NODE_ENV
+    )} mode on port ${chalk.blue.bold(PORT)}`
+  );
+});
